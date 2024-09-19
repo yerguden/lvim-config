87e4983c9ca74d5e0e466113ee1a0dcef15f7cce
Yüksel Ergüden <erguden@bryter.io>
2024-09-19 10:31:29 +0200

    update plugins

--- init.lua
+++ init.lua
@@ -2,22 +2,49 @@
 require("config.lazy")
 local lspconfig = require("lspconfig")
 
-lspconfig.tsserver.setup({
+-- lspconfig.ts_ls.setup({
+--   root_dir = require("lspconfig.util").root_pattern(".git"),
+-- })
+lspconfig.vtsls.setup({
   root_dir = require("lspconfig.util").root_pattern(".git"),
 })
 
-lspconfig.graphql.setup({
-  on_attach = function(client, bufnr)
-    -- Configuration specifics here
-    -- For example, keybindings or buffer-specific settings
-  end,
-  flags = {
-    debounce_text_changes = 150,
+lspconfig.eslint.setup({
+  root_dir = require("lspconfig.util").root_pattern(".git"),
+})
+
+lspconfig.tailwindcss.setup({
+  root_dir = require("lspconfig.util").root_pattern(".git"),
+  settings = {
+    tailwindCSS = {
+      includeLanguages = {
+        heex = "html",
+        eelixir = "html",
+        -- ex = "html",
+        elixir = "html",
+      },
+      experimental = {
+        classRegex = {
+          'class[:]\\s*"([^"]*)"',
+          "class[:]\\s*'([^']*)'",
+          "class[:]\\s*\\[([^\\]]*)\\]",
+          "class[:]\\s*{([^}]*)}",
+        },
+      },
+    },
   },
-  root_dir = function(fname)
-    return require("lspconfig.util").root_pattern(".git")(fname)
-      or require("lspconfig.util").root_pattern("package.json", "tsconfig.json", "jsconfig.json")(fname)
-      or require("lspconfig.util").path.dirname(fname)
-  end,
+})
+--
+local root_dir = vim.fn.getcwd()
+local schema_path = "/Users/yuko/legal-intake/deployables/api/schema.graphql"
+-- Configure GraphQL Language Server with schema path relative to project root
+lspconfig.graphql.setup({
+  root_dir = require("lspconfig.util").root_pattern(".git"),
+  settings = {
+    graphql = {
+      -- Use the root_dir combined with the relative path to your schema
+      schemaPath = schema_path,
+    },
+  },
 })
 vim.opt.shell = "/bin/bash"
