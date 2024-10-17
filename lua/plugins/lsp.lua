8b6b14c8f268d4a25618f41971651aa08b2a8775
Yüksel Ergüden <erguden@bryter.io>
2024-10-17 20:25:30 +0200

    changes

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         elixirls = {
+--           enabled = false, -- Disable elixir-ls if it keeps installing automatically
+--         },
+--       },
+--     },
+--   },
+-- }
